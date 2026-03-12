a90495d101de1aff925e443b5fdcc984e5cbc791
gilesagnel <gilesagnel@gmail.com>
2026-03-12 13:03:05 -0500

    fix alignment issue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <nav>
+  <div class="main">
+    <nav class="nav-bar">
       <router-link to="/">Chat</router-link>
       <router-link to="/dashboard">Dashboard</router-link>
       <router-link to="/profile">Profile</router-link>
       <router-link to="/history">History</router-link>
     </nav>
 
-    <router-view />
+    <div class="body">
+      <router-view />
+    </div>
+    
   </div>
 </template>
 
 <style>
-nav {
+.main {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.nav-bar
+ {
   display: flex;
   gap: 20px;
-  padding: 20px;
+  padding: 20px 40px;
 }
 </style>
